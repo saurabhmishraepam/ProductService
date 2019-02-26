77d6843f32aec36f2cabed85ff2d31b2bf4de16f
saurabh <mishra.saurabh19@gmail.com>
2019-02-27 00:19:39 +0530

    adding testcases

---
+++ src/test/java/com/epam/ProductsReview/restController/ProductsControllerTest.java
@@ -0,0 +1,7 @@
+package com.epam.ProductsReview.restController;
+
+/**
+ * Created by saurabh on 26/2/19.
+ */
+public class ProductsControllerTest {
+}
---
+++ src/test/java/com/epam/ProductsReview/service/ProductServiceTest.java
@@ -0,0 +1,7 @@
+package com.epam.ProductsReview.service;
+
+/**
+ * Created by saurabh on 26/2/19.
+ */
+public class ProductServiceTest {
+}
